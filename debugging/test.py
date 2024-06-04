f942d35edd43a0ddcdedf91259be1ec85fb61239
TLazarevic <lazarevict97@gmail.com>
2024-06-04 22:24:08 +0200

    Add test script

---
+++ debugging/test.py
@@ -0,0 +1,24 @@
+import airbyte as ab
+from airbyte_api.models import Mixpanel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_SECRET = os.environ["API_SECRET"]
+
+# TODO: Test all connection options
+
+# source_config = ProjectSecret(api_secret=API_SECRET)
+source = ab.get_source(
+    "source-mixpanel",
+    config={"credentials": {"api_secret": API_SECRET, "option_title": "Project Secret"}},
+    install_if_missing=True,
+)
+
+source.check()
+source.select_all_streams()
+result = source.read()
+
+for name, records in result.streams.items():
+    print(f"Stream {name}: {len(list(records))} records")
