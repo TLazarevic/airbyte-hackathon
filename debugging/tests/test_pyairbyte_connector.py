d14f954370f58fb8507807a943df9b73e4d7d76d
Miloš Tepić <tepcmils@gmail.com>
2024-06-16 13:11:37 +0200

    Assert source is freezed

--- debugging/tests/test_pyairbyte_connector.py
+++ debugging/tests/test_pyairbyte_connector.py
@@ -4,10 +4,20 @@ Breaking the limit of 60 requests per seconds freezes client
 """
 
 import pytest
-from clients import *
+from clients import pyairbyte_connector
+import threading
 
 
 def test_connection_hanging():
-    pair = pyairbyte_connector.source
+    pyairbyte_connector.select_streams(["cohorts"])
 
-    assert 3 == 3
+    thread = threading.Thread(target=pyairbyte_connector.read())
+
+    thread.start()
+    result = thread.join(timeout=10)
+
+    if thread.is_alive():
+        pytest.fail("pyairbyte connection is still trying to connect")
+    else:
+        # assert result, it should returns 429 HTTP code
+        assert result == 429
