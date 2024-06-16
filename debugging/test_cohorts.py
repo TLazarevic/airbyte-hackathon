3c1b258b821e364464595037fb5e57ef2980fd86
TLazarevic <lazarevict97@gmail.com>
2024-06-16 11:14:04 +0200

    remove redundant params

--- debugging/test_cohorts.py
+++ debugging/test_cohorts.py
@@ -31,11 +31,7 @@ def get_pyairbyte_cohorts():
                 "secret": os.environ["SECRET"],
                 "project_id": int(os.environ["PROJECT_ID"]),
             },
-            "start_date": "2023-01-01T00:00:00Z",
-            "end_date": "2023-01-01T00:10:00Z",
             "region": "EU",
-            "attribution_window": 0,
-            "date_window_size": 180,
         },
         install_if_missing=True,
     )
