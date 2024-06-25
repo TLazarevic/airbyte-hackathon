5754039ff31f3947e6f2dd76adb5924eb0efdff6
Miloš Tepić <tepcmils@gmail.com>
2024-06-25 19:17:14 +0200

    Format files

--- debugging/tests/clients.py
+++ debugging/tests/clients.py
@@ -8,7 +8,6 @@ import pandas as pd
 
 load_dotenv()
 
-API_SECRET = os.environ["API_SECRET"]
 USERNAME = os.environ["USERNAME"]
 SECRET = os.environ["SECRET"]
 PROJECT_ID = int(os.environ["PROJECT_ID"])
