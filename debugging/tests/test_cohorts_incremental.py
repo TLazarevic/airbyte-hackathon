09e456294c51428cc970b42fd35079b197c0d222
TLazarevic <lazarevict97@gmail.com>
2024-06-25 18:40:57 +0200

    Add debug statements

--- debugging/tests/test_cohorts_incremental.py
+++ debugging/tests/test_cohorts_incremental.py
@@ -39,6 +39,9 @@ class TestCohorts:
         source.select_streams(["cohorts"])
         pyairbyte_result = source.read()
         pyairbyte_result = pyairbyte_result["cohorts"].to_pandas()
+        
+        print(api_result)
+        print(pyairbyte_result)
 
         assert api_result.shape[0] == pyairbyte_result.shape[0]
         assert set(api_result["id"]) == set(pyairbyte_result["id"])
